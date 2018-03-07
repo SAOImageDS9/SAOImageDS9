fa6b5102a4a03920929120d2991413d7896e9d86
William Joye <wjoye@cfa.harvard.edu>
2018-03-07 16:04:45 -0500

    update ds9 skyview parser

--- ds9/library/error.tcl
+++ ds9/library/error.tcl
@@ -62,7 +62,7 @@ proc ParserError {msg yycnt yy_current_buffer index_} {
 	    Error "$msg: [lindex $yy_current_buffer [expr $yycnt-1]]"
 	}
 	default {
-	    puts stderr "$yy_current_buffer"
+	    puts stderr "[string range $yy_current_buffer 0 60]"
 	    puts stderr [format "%*s" $index_ ^]
 	    puts stderr "$msg:"
 	    QuitDS9

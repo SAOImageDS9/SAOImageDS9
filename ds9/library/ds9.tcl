5c4c64d0dd9ba76897614f080991119227df652c
William Joye <wjoye@cfa.harvard.edu>
2019-12-18 16:30:18 -0500

    update version number

--- ds9/library/ds9.tcl
+++ ds9/library/ds9.tcl
@@ -13,9 +13,9 @@ proc DS9Def {} {
     }
 
     # Must be major,minor
-    set ds9(version) {8.1}
+    set ds9(version) {8.2}
     # For display purposes only
-    set ds9(version,display) {8.1}
+    set ds9(version,display) {8.2b1}
 
     set ds9(top) .
     set ds9(mb) .mb

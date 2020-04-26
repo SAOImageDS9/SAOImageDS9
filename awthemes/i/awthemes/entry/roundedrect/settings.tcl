5821883d4f85303e79cf006c67bad8e911c6a50e
William Joye <wjoye@cfa.harvard.edu>
2020-04-26 11:40:21 -0400

    awthemes

---
+++ awthemes/i/awthemes/entry/roundedrect/settings.tcl
@@ -0,0 +1,6 @@
+set colors(entry.image.border) 6
+set colors(entry.image.padding) {5 8}
+set colors(spinbox.image.border) 6
+set colors(spinbox.image.padding) {4 4}
+set colors(combobox.entry.image.border) 6
+set colors(combobox.entry.image.padding) {6 5}
---
+++ awthemes/i/awthemes/progressbar/rect/settings.tcl
@@ -0,0 +1,2 @@
+set colors(slider.image.border) {1 0}
+set colors(trough.image.border) {1 0}

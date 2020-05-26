c0850efe0fd651fbab511401a97de241538169f1
William Joye <wjoye@cfa.harvard.edu>
2020-05-26 12:00:22 -0400

    FreeBSD changes

--- fitsy/alloc.C
+++ fitsy/alloc.C
@@ -3,6 +3,8 @@
 // For conditions of distribution and use, see copyright notice in "copyright"
 
 #include <stdio.h>
+// FreeBSD
+#include <unistd.h>
 #include "alloc.h"
 
 FitsAlloc::FitsAlloc(const char* fn)

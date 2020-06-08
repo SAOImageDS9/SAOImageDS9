f156aeaa650afa0ff3d82692aaed461d5de2d0b4
William Joye <wjoye@cfa.harvard.edu>
2020-06-08 17:01:31 -0400

    tweak themes

--- ds9/library/mfile.tcl
+++ ds9/library/mfile.tcl
@@ -23,9 +23,8 @@ proc FileMainMenu {} {
 	-menu $ds9(mb).file.import
     $ds9(mb).file add cascade -label [msgcat::mc {Export}] \
 	-menu $ds9(mb).file.export
-    $ds9(mb).file add separator
-    $ds9(mb).file add command -label "[msgcat::mc {Prism}]..." \
-	-command PrismDialog
+#    $ds9(mb).file add separator
+#    $ds9(mb).file add command -label "[msgcat::mc {Prism}]..." -command PrismDialog
     $ds9(mb).file add separator
     $ds9(mb).file add cascade -label [msgcat::mc {Save Image}] \
 	-menu $ds9(mb).file.saveimage

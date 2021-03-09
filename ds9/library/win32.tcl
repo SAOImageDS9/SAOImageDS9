e7faf1fe16c5f8d7cd8d8860b1232e4768c0a234
William Joye <wjoye@cfa.harvard.edu>
2021-03-09 15:45:14 -0500

    remove old macos print code

--- ds9/library/win32.tcl
+++
@@ -1,6 +0,0 @@
-#  Copyright (C) 1999-2018
-#  Smithsonian Astrophysical Observatory, Cambridge, MA, USA
-#  For conditions of distribution and use, see copyright notice in "copyright"
-
-package provide DS9 1.0
-

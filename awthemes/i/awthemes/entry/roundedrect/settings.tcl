64ce720f188a4cc2c780ef0ba3fb5b4e9eaea9b2
William Joye <wjoye@cfa.harvard.edu>
2021-06-04 15:41:19 -0400

    update awthemes

--- awthemes/i/awthemes/entry/roundedrect/settings.tcl
+++
@@ -1,6 +0,0 @@
-set colors(entry.image.border) 6
-set colors(entry.image.padding) {5 8}
-set colors(spinbox.image.border) 6
-set colors(spinbox.image.padding) {4 4}
-set colors(combobox.entry.image.border) 6
-set colors(combobox.entry.image.padding) {6 5}

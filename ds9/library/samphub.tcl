1e4b40256c4c1e9bb9bada7d4cdf75cfff466f0f
William Joye <wjoye@cfa.harvard.edu>
2023-05-25 17:44:49 -0400

    samphub

--- ds9/library/samphub.tcl
+++ ds9/library/samphub.tcl
@@ -1,4 +1,4 @@
-#  Copyright (C) 1999-2021
+#  Copyright (C) 1999-2023
 #  Smithsonian Astrophysical Observatory, Cambridge, MA, USA
 #  For conditions of distribution and use, see copyright notice in "copyright"
 

58a03b2c874b09656a2de3793de99a2dee680191
William Joye <wjoye@cfa.harvard.edu>
2024-04-06 16:56:52 -0400

    add support for WCS LONG/NPOL binning

--- fitsy/hist.C
+++ fitsy/hist.C
@@ -671,6 +671,50 @@ void FitsHist::initWCS(FitsFile* fits, Matrix& mm, Vector block)
     // general
     mapWCSString(srcHead, w, "BUNIT", "TUNIT");
   }
+
+  // do we have a LONG-TAN/NPOL-TAN CTYPE?
+  if (head_->find("CTYPE1") && head_->find("CTYPE2")) {
+    char* cc;
+    int ff1 =0;
+    int ff2 =0;
+
+    cc = head_->getString("CTYPE1");
+    if (!strncmp(cc,"LONG-",5)) {
+      char str[9];
+      strncpy(str,"XLON-",5);
+      strncpy(str+5,cc+5,3);
+      str[8] = '\0';
+      head_->setString("CTYPE1",str,"");
+      ff1 =1;
+    }
+
+    cc = head_->getString("CTYPE2");
+    if (!strncmp(cc,"NPOL-",5)) {
+      char str[9];
+      strncpy(str,"XLAT-",5);
+      strncpy(str+5,cc+5,3);
+      str[8] = '\0';
+      head_->setString("CTYPE2",str,"");
+      ff2 =1;
+    }
+
+    if (ff1 && ff2) {
+      if (head_->find("CRVAL2")) {
+	float val = head_->getReal("CRVAL2",0) + 90;
+	head_->setReal("CRVAL2",val,5,"");
+      }
+    }
+  }
+
+  if (1) {
+    char* cptr = head_->cards();
+    for (int i=0; i<head_->ncard(); i++,cptr+=FTY_CARDLEN) {
+      char cc[FTY_CARDLEN];
+      memcpy(cc, cptr, FTY_CARDLEN);
+      cc[FTY_CARDLEN-1] = '\0';
+      cerr << cc << endl;
+    }
+  }
 }
 
 FitsHistNext::FitsHistNext(FitsFile* prev)

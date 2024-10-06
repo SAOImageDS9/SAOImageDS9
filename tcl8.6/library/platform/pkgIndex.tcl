aa9ab1cdd2ab83d0ad58dc83525cf1549c6d6c9d
William Joye <wjoye@cfa.harvard.edu>
2024-10-06 14:19:30 -0400

    update tcl/tk 8.6.15

---
+++ tcl8.6/library/platform/pkgIndex.tcl
@@ -0,0 +1,3 @@
+package ifneeded platform        1.0.19 [list source -encoding utf-8 [file join $dir platform.tcl]]
+package ifneeded platform::shell 1.1.4 [list source -encoding utf-8 [file join $dir shell.tcl]]
+

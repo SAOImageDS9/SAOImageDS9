03dc7b2caaabd6dc8dd3d539ea810f04b023633f
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2026-05-22 12:20:15 -0400

    ttkthemes: update to 3.3.0

--- ttkthemes/ttkthemes/png/pkgIndex.tcl
+++ ttkthemes/ttkthemes/png/pkgIndex.tcl
@@ -2,7 +2,7 @@
 # License: GNU GPLv3
 # Copyright (c) 2017-2020 RedFantom
 
-package require Tk 8.6
+package require Tk 8.6-
 set png_theme_dir [file join [pwd] [file dirname [info script]]]
 
 array set png_themes {
@@ -12,7 +12,7 @@ array set png_themes {
   equilux 1.1
   scid 0.9.1
   ubuntu 1.0
-  yaru 0.1
+  yaru 1.2
 }
 
 foreach {theme version} [array get png_themes] {

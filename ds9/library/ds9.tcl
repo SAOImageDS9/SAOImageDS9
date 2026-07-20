28d6ef5b1917cdb866dfda3e42014edf86be0b6f
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2026-07-20 07:24:17 -0400

    DS9: Init 8.8b2 dev

--- ds9/library/ds9.tcl
+++ ds9/library/ds9.tcl
@@ -9,7 +9,7 @@ proc DS9Def {} {
     # Must be major,minor
     set ds9(version) {8.8}
     # For display purposes only
-    set ds9(version,display) {8.8b1}
+    set ds9(version,display) {8.8b2}
 
     set ds9(visual) {}
     set ds9(depth) 8

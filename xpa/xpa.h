5e824355d990ed2a5a91237f0c4230c8adc399d0
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2026-07-21 07:05:41 -0400

    XPA: bump up version to 2.2.0

--- xpa/xpa.h
+++ xpa/xpa.h
@@ -19,9 +19,9 @@
 #endif
 
 #define XPA_MAJOR_VERSION 2
-#define XPA_MINOR_VERSION 1
-#define XPA_PATCH_LEVEL 21
-#define XPA_VERSION "2.1.21"
+#define XPA_MINOR_VERSION 2
+#define XPA_PATCH_LEVEL 0
+#define XPA_VERSION "2.2.0"
 
 /* #define XPA_DEBUG 1 */
 #ifdef XPA_DEBUG
